c8e342be6b53d21f5bbe1c2a1c5ce5c22816b323
Eren <erendore38k@gmail.com>
2023-11-16 15:09:48 +0300

    The algorithm that gives the sum of two numbers was done with the third method.

---
+++ SumOfTwoNumbers/SumOfTwoNumbers3/Program.cs
@@ -0,0 +1,9 @@
+﻿//Enter the algorithm that returns the sum of two numbers.
+
+Console.WriteLine("Please enter the first number.");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter the second number.");
+int number2  = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(number1 + number2);
