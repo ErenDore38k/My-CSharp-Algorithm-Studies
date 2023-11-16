2088779dc0a61ac87a90382ec00d5d71941f47a5
Eren <erendore38k@gmail.com>
2023-11-16 20:04:45 +0300

    The algorithm that gives the sum of squares of two numbers was done with the first method.

---
+++ SumOfSquaresOfTwoNumbers/SumOfSquaresOfTwoNumbers1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Write an application that returns the sum of the squares of two numbers entered by the user.
+
+Console.WriteLine("Please enter the first number.");
+int number1 = Convert.ToInt32(Console.ReadLine());
+number1 = number1 * number1;
+
+Console.WriteLine("Please enter the second number.");
+int number2 = Convert.ToInt32(Console.ReadLine());
+number2 = number2 * number2;
+
+Console.WriteLine(number1 + number2);
