d2cdfda105e80b9259eee3689805ed9854f6fc79
Eren <erendore38k@gmail.com>
2023-11-17 11:34:53 +0300

    Finding the sum of the cubes of numbers was done by the fourth method.

---
+++ FindingTheSumOfTheCubesOfNumbers/FindingTheSumOfTheCubesOfNumbers4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Write an application that finds the sum of the cubes of the numbers 1 to 10.
+
+//Solution with Ternary Operator
+ 
+            
+int i = 1;
+double conclusion = 0;
+
+while (i <= 10)
+{
+    conclusion += Math.Pow(i, 3);
+    Console.Write(i == 10 ? $"{i}³ = {conclusion}" : $"{i}³ + ");
+    i++;
+}
