c1e4b31e64edfba09bc3156ee0bf802a29be0f48
Eren <erendore38k@gmail.com>
2023-11-17 11:42:42 +0300

    Finding the sum of the cubes of numbers was done by the sixth method.

---
+++ FindingTheSumOfTheCubesOfNumbers/FindingTheSumOfTheCubesOfNumbers6/Program.cs
@@ -0,0 +1,7 @@
+﻿//Write an application that finds the sum of the cubes of the numbers 1 to 10.
+
+//It can be finished in a single line with the Gauss method,
+
+Console.Write(Math.Pow(((10 * (10 + 1)) / 2), 2));
+
+
