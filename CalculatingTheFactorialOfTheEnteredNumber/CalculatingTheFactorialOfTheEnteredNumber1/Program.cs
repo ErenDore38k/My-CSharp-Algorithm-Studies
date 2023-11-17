5713e575f2e638279a6d9ee2d1b2d4c2fa31cf95
Eren <erendore38k@gmail.com>
2023-11-17 20:04:03 +0300

    Calculation of the factorial of the entered number was done with the first method.

---
+++ CalculatingTheFactorialOfTheEnteredNumber/CalculatingTheFactorialOfTheEnteredNumber1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Write an application that calculates the factorial of the entered number.
+
+Console.Write("Please enter a number:");
+int number = Convert.ToInt32(Console.ReadLine());
+int factorial = 1;
+for (int i = 1; i <= number; i++)
+{
+    factorial *= i;
+}
+Console.WriteLine(number + "! = " + factorial);
+
+Console.ReadLine();
