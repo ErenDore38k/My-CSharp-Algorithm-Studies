a6bd796a5e6153fbffeb09927c4a512b3e2c84a9
Eren <erendore38k@gmail.com>
2023-11-17 14:32:28 +0300

    The algorithm that calculates the person's age was made with the first method

---
+++ AgeCalculationBasedOnDateOfBirth/AgeCalculationBasedOnDateOfBirth1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Write the algorithm that calculates the age of the person whose date of birth is entered.
+
+
+Console.WriteLine("Please write your date of birth.");
+DateTime dateofbirth = Convert.ToDateTime(Console.ReadLine());
+
+TimeSpan result = DateTime.Now - dateofbirth;
+Console.WriteLine(result.Days / 365);
+
+
+
