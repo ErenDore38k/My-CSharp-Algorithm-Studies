63d6a35ddb05d989c1562a95775fb71bbe3ea587
Eren <erendore38k@gmail.com>
2023-11-18 08:36:38 +0300

    Learning the factorial of a number was done with the 2nd method.

---
+++ CalculatingTheFactorialOfANumber/CalculatingTheFactorialOfANumber2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Write the algorithm that calculates the factorial of the entered number.
+
+Console.Write("Enter a number");
+int number = Convert.ToInt32(Console.ReadLine());
+int factorial = 1;
+while (number > 1)
+{
+    factorial *= number;
+    number--;
+}
+Console.WriteLine("factorial = " + factorial);
