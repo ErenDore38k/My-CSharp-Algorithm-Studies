73728faa0792bcacc286bf5e20bd0f4cf335e84e
Eren <erendore38k@gmail.com>
2023-11-18 18:08:48 +0300

    Multiplication of positive numbers was done by the second method.

---
+++ MultiplyingPositiveNumbers/MultiplyingPositiveNumbers2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Write an algorithm to find multiplication of positive numbers using addition.
+
+Console.WriteLine("Please enter a positive number.");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter a positive number.");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int asaresultofimpact =  number1*number2;
+int additionresult = asaresultofimpact ;
+
+Console.WriteLine($"{number1} * {number2}  = {asaresultofimpact}");
+
+
