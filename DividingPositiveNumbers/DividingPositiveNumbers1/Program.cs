de9cef0c7f55fb372b60af72e3a67d6be4eca856
Eren <erendore38k@gmail.com>
2023-11-19 16:17:14 +0300

    Division of positive numbers was done by the first method.

---
+++ DividingPositiveNumbers/DividingPositiveNumbers1/Program.cs
@@ -0,0 +1,31 @@
+﻿//Write an algorithm that performs division of positive numbers using subtraction.
+
+//remainder = kalan
+//conclusion = sonuç 
+
+Console.WriteLine("Please enter the number you want to divide.");
+double number1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Please enter the number you will divide.");
+double number2  = Convert.ToDouble(Console.ReadLine());
+
+double _number1  = number1;
+
+
+double conclusion= 0;
+double remainder = 0;
+
+
+for (int i = 0; i <_number1; i++)
+{
+    number1 -= number2;
+    conclusion++;
+    if (number1 < number2)
+    {
+        remainder = number1;
+        break;
+    }
+        
+}
+Console.WriteLine($"{_number1}/{number2} = {conclusion} | remainder = {remainder}");
+
