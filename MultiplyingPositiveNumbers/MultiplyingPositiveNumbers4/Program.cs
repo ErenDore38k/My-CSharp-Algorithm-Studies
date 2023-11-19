e90b92502845be8e0b2ec06646b8fda065743325
Eren <erendore38k@gmail.com>
2023-11-19 13:16:27 +0300

    Multiplication of positive numbers was done by the fourth method.

---
+++ MultiplyingPositiveNumbers/MultiplyingPositiveNumbers4/Program.cs
@@ -0,0 +1,22 @@
+﻿//Write an algorithm to find multiplication of positive numbers using addition.
+
+//counter = sayaç
+//conclusion = sonuç
+
+Console.WriteLine("Please enter the first number.");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter the second number.");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int counter = number2;
+int conclusion = 0;
+
+do
+{
+    conclusion += number1;
+    counter--;
+} while (counter > 0);
+
+Console.WriteLine($"{number1}x{number2} = {conclusion}");
+
