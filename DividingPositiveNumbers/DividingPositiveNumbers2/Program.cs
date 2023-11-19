df98b05406cf2c4301393c3ac0dd24883fc595fd
Eren <erendore38k@gmail.com>
2023-11-19 20:11:51 +0300

    Division of positive numbers was done by the second method.

---
+++ DividingPositiveNumbers/DividingPositiveNumbers2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Write an algorithm that performs division of positive numbers using subtraction.
+
+//counter = sayaç
+//counclusion = sonuc
+
+Console.WriteLine("Please enter the divisor number.");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter the partition value.");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int _number1 = number1;
+int counclusion = 0, remainder =0;
+
+while (true)
+{
+    number1 -= number2;
+    counclusion++;
+    if (number1 < number2)
+    {
+        remainder = number1;
+        break;
+
+    }
+        
+}
+Console.WriteLine($"{_number1} / {number2} = {counclusion} | remainder = {remainder}");
