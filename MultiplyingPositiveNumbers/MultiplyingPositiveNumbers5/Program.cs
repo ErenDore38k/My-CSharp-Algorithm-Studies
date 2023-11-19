6799d0e2e067064ad6ddace7a5a9dbb8f3a30e3e
Eren <erendore38k@gmail.com>
2023-11-19 14:05:05 +0300

    Multiplication of positive numbers was done by the fifth method.

---
+++ MultiplyingPositiveNumbers/MultiplyingPositiveNumbers5/Program.cs
@@ -0,0 +1,15 @@
+﻿//Write an algorithm to find multiplication of positive numbers using addition.
+
+Console.WriteLine("Please enter the first number");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter the second number");
+int number2  = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Gather(number1, number2));
+static int Gather(int number1,int number2)
+{
+    if (number2 > 1)
+        return number1 + Gather(number1, --number2);
+    return number1;
+}
