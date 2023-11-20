5a5ac70533732d9fffa64a4c67a0d6eee0998325
Eren <erendore38k@gmail.com>
2023-11-20 20:41:21 +0300

    Finding out how many digits the number has was done with the 6th method.

---
+++ LearningHowManyDigitsANumberHas/LearningHowManyDigitsANumberHas6/Program.cs
@@ -0,0 +1,20 @@
+﻿//Write an application that tells you how many digits the entered positive number has.
+
+//conclusion = sonuç
+
+Console.WriteLine("Please enter the number you want to know the number of digits.");
+int number = int.Parse(Console.ReadLine());
+int conclusion = NumberOfDigits(number);
+NumberOfDigits(number);
+if (number < 10)
+    Console.WriteLine($"{number}  = {conclusion}");
+else Console.WriteLine($"{number} number of digits of the number = {++conclusion}");
+
+static int NumberOfDigits(int number)
+{
+
+    number /= 10;
+    if (number >= 10)
+        return 1 + NumberOfDigits(number);
+    return 1;
+}
