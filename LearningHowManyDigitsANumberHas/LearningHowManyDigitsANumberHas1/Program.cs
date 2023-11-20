bfc312aab0a640afc5f4797e658b6a03b8b7170e
Eren <erendore38k@gmail.com>
2023-11-20 12:10:44 +0300

    Learning how many digits the number is was done with the 1st method.

---
+++ LearningHowManyDigitsANumberHas/LearningHowManyDigitsANumberHas1/Program.cs
@@ -0,0 +1,37 @@
+﻿//Write an application that tells you how many digits the entered positive number has.
+
+Console.WriteLine("Please enter the number you want to know the number of digits.");
+ Double number = Double.Parse(Console.ReadLine());
+
+#region method1
+/*
+
+for (double i = 0;number >= 10;)
+		{
+	number /= 10;
+	counter++;
+	number++;
+}
+*/
+#endregion
+
+#region method2
+/*
+for (double i = 0; number>=10; i++)
+{
+	number /= 10;
+}
+*/
+#endregion
+
+int counter = 0;
+
+for(; number >= 10;)
+{
+	number/= 10;
+	counter++;
+}
+counter++; //We increase the counter because it does not give the place value one less value.
+ 
+
+Console.WriteLine($"{number} place value of number {counter}");
