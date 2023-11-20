f8add6d9770772ff2c20d50bbc868bce18d2a4ed
Eren <erendore38k@gmail.com>
2023-11-20 13:32:57 +0300

    Learning how many digits the number is was done with the 2st method.

---
+++ LearningHowManyDigitsANumberHas/LearningHowManyDigitsANumberHas2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Write an application that tells you how many digits the entered positive number has.
+
+//This time it will be with an infinite loop.
+// conclusion = sonuç
+
+Console.WriteLine("Please enter the number you want to know the number of digits.");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int conclusion = 0;
+int _number = number;
+for (; ; )  //In this way, an infinite loop can be created. 
+{
+    number /= 10;
+    conclusion++;
+    if (number < 10)
+        break;
+}
+conclusion++;
+Console.WriteLine($"{_number} the number {conclusion} place value of number");
